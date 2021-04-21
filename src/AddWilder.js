d0cfcdb6ba8c85925906334385a27f5e62698a08
Karim Makhloufi <maclouf@gmail.com>
2021-04-21 19:24:41 +0200

    Added graphql way of posting data

--- src/AddWilder.js
+++ src/AddWilder.js
@@ -1,15 +1,37 @@
 import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
 import { Button, Form, Input, Label } from "./styles/form-elements";
+
+const CREATE_WILDER = gql`
+  mutation CreateWilder($input: InputWilder!) {
+    createWilder(input: $input) {
+      id
+      name
+      city
+    }
+  }
+`;
 
 function AddWilder() {
   const [name, setName] = useState("");
   const [city, setCity] = useState("");
+  const [createWilder, { data }] = useMutation(CREATE_WILDER);
+
   return (
     <Form
       onSubmit={async (e) => {
         e.preventDefault();
+        createWilder({
+          variables: {
+            input: {
+              name,
+              city,
+            },
+          },
+        });
       }}
     >
+      {data && <p>wilder {data.createWilder.name} a été ajouté.e</p>}
       <Label htmlFor="name-input">Name :</Label>
       <Input
         id="name-input"
