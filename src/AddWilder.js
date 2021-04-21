c464fca7293cb5f1316abc95f46250e761053c43
Karim Makhloufi <maclouf@gmail.com>
2021-04-21 19:21:11 +0200

    Removed old way of creating wilder

--- src/AddWilder.js
+++ src/AddWilder.js
@@ -1,34 +1,13 @@
 import React, { useState } from "react";
-import axios from "axios";
-import { Button, Error, Form, Input, Label } from "./styles/form-elements";
+import { Button, Form, Input, Label } from "./styles/form-elements";
 
 function AddWilder() {
   const [name, setName] = useState("");
   const [city, setCity] = useState("");
-  const [error, setError] = useState("");
   return (
     <Form
       onSubmit={async (e) => {
         e.preventDefault();
-        try {
-          const result = await axios.post(
-            "http://localhost:5000/api/wilders",
-            {
-              name,
-              city,
-            }
-          );
-          console.log(result);
-          if (result.data.success) {
-            setError("");
-          }
-        } catch (error) {
-          if (error.response) {
-            setError(error.response.data.message);
-          } else {
-            setError(error.message);
-          }
-        }
       }}
     >
       <Label htmlFor="name-input">Name :</Label>
@@ -47,7 +26,6 @@ function AddWilder() {
         value={city}
         onChange={(e) => setCity(e.target.value)}
       />
-      {error !== "" && <Error>{error}</Error>}
       <Button>Add</Button>
     </Form>
   );
